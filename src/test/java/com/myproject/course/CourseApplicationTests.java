6d24ab2429d02ddbb4b077621a3e246f0db56f23
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2021-05-20 11:21:25 -0300

    Projeto criado

---
+++ src/test/java/com/myproject/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
