203bcde0612bff84b1a8951a7a9de6dd7639000a
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2021-05-20 18:38:44 -0300

    Camada de servico

--- src/main/java/com/myproject/course/resources/UserResource.java
+++ src/main/java/com/myproject/course/resources/UserResource.java
@@ -1,20 +1,36 @@
 package com.myproject.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.myproject.course.entities.User;
+import com.myproject.course.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource 
 {
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll()
+	public ResponseEntity<List<User>> findAll()
 	{
-		User u = new User(1L, "Maria", "maria@gmail.com", "1159275937", "mariazinha9191");
-		return ResponseEntity.ok().body(u);
+		List<User> list = service.findAll();
+				
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id)
+	{
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
