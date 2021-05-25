6f8375590730f894b561c16ad93891c297668f86
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2021-05-25 14:27:17 -0300

    Entidade payment

--- src/main/java/com/myproject/course/config/TestConfig.java
+++ src/main/java/com/myproject/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.myproject.course.entities.Category;
 import com.myproject.course.entities.Order;
 import com.myproject.course.entities.OrderItem;
+import com.myproject.course.entities.Payment;
 import com.myproject.course.entities.Product;
 import com.myproject.course.entities.User;
 import com.myproject.course.entities.enums.OrderStatus;
@@ -87,5 +88,9 @@ public class TestConfig implements CommandLineRunner
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	} 
 }
